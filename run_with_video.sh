547c98303d0d38fa0d7546b4fd12dc85afaa8c66
AJ Bensman <aj.bensman@cfacorp.com>
2021-06-23 15:42:13 -0400

    generalized shell commands to use arguments

--- run_with_video.sh
+++ run_with_video.sh
@@ -4,4 +4,4 @@ cp -r /home/gst-plugins-tf/data/models/mymodel_$1/ /home/gst-plugins-tf/data/mod
 
 source venv/bin/activate
 export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:$PWD/venv/lib/gstreamer-1.0/:$PWD/gst/
-GST_DEBUG=python:5 gst-launch-1.0 rtspsrc location=rtsp://admin:$2@192.168.129.110/profile2/media.smp ! queue ! rtph264depay ! decodebin ! videoconvert !  gst_tf_detection config=data/tf_object_api_cfg.yml ! videoconvert !  gst_detection_overlay ! videoconvert ! autovideosink
+GST_DEBUG=python:5 gst-launch-1.0 rtspsrc location=rtsp://$2:$3@$4/profile2/media.smp ! queue ! rtph264depay ! decodebin ! videoconvert !  gst_tf_detection config=data/tf_object_api_cfg.yml ! videoconvert !  gst_detection_overlay ! videoconvert ! autovideosink
--- startup.sh
+++
@@ -1,2 +0,0 @@
-echo "argument is $1"
-mv /home/gst-plugins-tf/requirements.txt /home/gst-plugins-tf/req.txt
