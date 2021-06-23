94fc7d82fbae576fa252cb70728db5899f4669a7
AJ Bensman <aj.bensman@cfacorp.com>
2021-06-23 14:18:15 -0400

    Added shell files to start up the detection

---
+++ run_with_video.sh
@@ -0,0 +1,7 @@
+# move chosen model to correct file location
+rm -r /home/gst-plugins-tf/data/models/mymodel/
+cp -r /home/gst-plugins-tf/data/models/mymodel_$1/ /home/gst-plugins-tf/data/models/mymodel/
+
+source venv/bin/activate
+export GST_PLUGIN_PATH=$GST_PLUGIN_PATH:$PWD/venv/lib/gstreamer-1.0/:$PWD/gst/
+GST_DEBUG=python:5 gst-launch-1.0 rtspsrc location=rtsp://admin:$2@192.168.129.110/profile2/media.smp ! queue ! rtph264depay ! decodebin ! videoconvert !  gst_tf_detection config=data/tf_object_api_cfg.yml ! videoconvert !  gst_detection_overlay ! videoconvert ! autovideosink
